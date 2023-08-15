deb4fb48d05cf26e2ac66535c2d34700c299ed92
Rokas <rookas.rudzenskas@gmail.com>
2023-08-15 17:38:24 +0300

    🔧 chore(train.ts): remove unnecessary whitespace to improve code readability

--- src/pages/api/projects/[id]/train.ts
+++ src/pages/api/projects/[id]/train.ts
@@ -21,6 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     },
   });
 
+
   const instanceClass = getRefinedInstanceClass(project.instanceClass);
 
   const responseReplicate = await replicateClient.post(

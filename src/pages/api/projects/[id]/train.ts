f6505c9cf84adc830edd6a731983d8d5f6f77c4c
Rokas <rookas.rudzenskas@gmail.com>
2023-08-15 17:39:08 +0300

    🔥 refactor(train.ts): remove unnecessary empty line in train.ts file to improve code readability

--- src/pages/api/projects/[id]/train.ts
+++ src/pages/api/projects/[id]/train.ts
@@ -21,7 +21,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     },
   });
 
-
   const instanceClass = getRefinedInstanceClass(project.instanceClass);
 
   const responseReplicate = await replicateClient.post(

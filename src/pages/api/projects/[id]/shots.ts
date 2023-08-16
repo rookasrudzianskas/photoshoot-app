d71b629279fd899a5dc4394593ef32112df4ab40
Rokas <rookas.rudzenskas@gmail.com>
2023-08-16 15:24:28 +0300

    🔥 refactor(shots.ts): remove unnecessary empty line at the top of the file to improve code readability

--- src/pages/api/projects/[id]/shots.ts
+++ src/pages/api/projects/[id]/shots.ts
@@ -2,6 +2,7 @@ import db from "@/core/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req });
   const projectId = req.query.id as string;
